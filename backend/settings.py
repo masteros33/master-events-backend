75f8c8ffce6b84735cf4ef362723adb03dc284a9
jude Obodai Sai <judeobodaisai@gmail.com>
2026-05-19 19:33:00 +0600

    Fix: read email settings from env vars

--- backend/settings.py
+++ backend/settings.py
@@ -300,14 +300,15 @@ PAYSTACK_SECRET_KEY = os.getenv('PAYSTACK_SECRET_KEY', '')
 PAYSTACK_PUBLIC_KEY = os.getenv('PAYSTACK_PUBLIC_KEY', '')
 BACKEND_URL         = os.getenv('BACKEND_URL', 'https://master-events-backend.onrender.com')
 
-# Email
-EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
-EMAIL_USE_TLS       = True
-EMAIL_HOST_USER     = os.getenv('EMAIL_HOST_USER', '')
+# Email Settings
+EMAIL_BACKEND      = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST         = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT         = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_USE_TLS      = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER    = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL', 'Master Events <mastereventgh@gmail.com>')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Master Events <mastereventgh@gmail.com>')
+
 
 # Admin email for CRITICAL log alerts
 ADMINS = [('Master Events Admin', os.getenv('ADMIN_EMAIL', 'mastereventgh@gmail.com'))]
